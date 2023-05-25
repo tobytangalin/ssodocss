eddf2143c9a29ae98ea2e7b4333c168a4f6a77e5
Toby Tangalin <97184147+tobytangalin@users.noreply.github.com>
2023-05-25 17:17:03 +0100

    here we go
    yeahh

--- en/js/toc.js
+++ en/js/toc.js
@@ -1,4 +1,54 @@
 var toc = [{
+"url": "introduction-to-the-sitecore-cloud-portal.html",
+"text": "Introduction to the Sitecore Cloud Portal",
+"class": "topic-link section"
+,
+"children": [
+{
+"url": "access-the-portal-for-the-first-time.html",
+"text": "Access the portal for the first time",
+"class": "topic-link section"
+},
+{
+"url": "log-in-to-the-sitecore-cloud-portal.html",
+"text": "Log in to the Sitecore Cloud Portal",
+"class": "topic-link section"
+},
+{
+"url": "access-sitecore-apps.html",
+"text": "Access Sitecore apps",
+"class": "topic-link section"
+},
+{
+"url": "access-another-organization.html",
+"text": "Access another organization",
+"class": "topic-link section"
+},
+{
+"url": "change-your-sitecore-password.html",
+"text": "Change your Sitecore password",
+"class": "topic-link section original-topic"
+,
+"children": [
+{
+"url": "reset-your-sitecore-cloud-portal-password.html",
+"text": "Reset your Sitecore Cloud Portal password",
+"class": "topic-link section"
+}]
+}]
+},
+{
+"url": "portal-administration.html",
+"text": "Portal administration",
+"class": "topic-link section"
+,
+"children": [
+{
+"url": "managing-access-in-the-portal.html",
+"text": "Managing access in the portal",
+"class": "topic-link section"
+},
+{
 "url": "single-sign-on--sso-.html",
 "text": "Single sign-on (SSO)",
 "class": "topic-link section"
@@ -12,6 +62,32 @@ var toc = [{
 {
 "url": "troubleshooting-faq.html",
 "text": "Troubleshooting FAQ",
+"class": "topic-link section"
+}]
+},
+{
+"url": "invite-team-members-to-your-sitecore-cloud-portal-organization.html",
+"text": "Invite team members to your Sitecore Cloud Portal organization",
+"class": "topic-link section"
+},
+{
+"url": "manage-the-access-of-a-team-member.html",
+"text": "Manage the access of a team member",
+"class": "topic-link section"
+},
+{
+"url": "export-team-member-data.html",
+"text": "Export team member data",
+"class": "topic-link section"
+},
+{
+"url": "manage-multi-factor-authentication.html",
+"text": "Manage multi-factor authentication",
+"class": "topic-link section"
+},
+{
+"url": "create-a-support-case.html",
+"text": "Create a support case",
 "class": "topic-link section"
 }]
 }];
